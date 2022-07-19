57e0d0554c0f9b4294e4b4b2a702db3a5dbaf27e
MohamedHamisa <108889205+MohamedHamisa@users.noreply.github.com>
2022-07-20 00:19:09 +0200

    Create Alexa.py

---
+++ Alexa.py
@@ -0,0 +1,64 @@
+!pip install SpeechRecognition 
+!pip install pywhatkit  #Pywhatkit is one of the popular python libraries for sending messages automatically to someone's WhatsApp mobile number
+!pip install pyttsx3    #text_to_speech conversion library 
+!pip install pyjokes 
+#is a python library that is used to create one-line jokes for programmers. 
+#Informally, it can also be referred as a fun python library which is pretty simple to use. Let us see how you can actually use it to perform the required task
+import speech_recognition as sr    
+import pyttsx3
+import pywhatkit
+import datetime 
+import wikipedia        #Python library that makes it easy to access and parse data from Wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
